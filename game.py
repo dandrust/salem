cb4488ccd2cd687c114da34f0666092a6686d220
Dan Drust <dandrust@gmail.com>
2024-02-12 17:10:23 -0500

    add second task - class conversion

---
+++ game.py
@@ -0,0 +1,6 @@
+"""
+  TODO: Use the functions in the `game_setup_script.py` to implement a Game class that we can
+  instantiate with the number of players and then call some methods to get the number of cards required
+
+  See `game_test.py` for the expected interface of the Game class.
+"""
---
+++ game_test.py
@@ -0,0 +1,29 @@
+from game import Game
+
+if __name__ == "__main__":
+  game = Game(2)
+  assert game.not_a_witch_cards_required() == 18
+  assert game.witch_cards_required() == 1
+  assert game.constable_cards_required() == 1
+
+  game = Game(5)
+  assert game.not_a_witch_cards_required() == 23
+  assert game.witch_cards_required() == 1
+  assert game.constable_cards_required() == 1
+
+  game = Game(8)
+  assert game.not_a_witch_cards_required() == 29
+  assert game.witch_cards_required() == 2
+  assert game.constable_cards_required() == 1
+
+  game = Game(11)
+  assert game.not_a_witch_cards_required() == 30
+  assert game.witch_cards_required() == 2
+  assert game.constable_cards_required() == 1
+
+  game = Game(12)
+  assert game.not_a_witch_cards_required() == 33
+  assert game.witch_cards_required() == 2
+  assert game.constable_cards_required() == 1
+  
+  print("All tests pass!")
