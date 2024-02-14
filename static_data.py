ab2c310838a8381e3d4b8ca811620bbf866c0928
Dan D <dandrust@gmail.com>
2024-02-13 22:42:45 -0500

    write simulation 1

---
+++ static_data.py
@@ -0,0 +1,17 @@
+TOWN_HALL_CARD_DATA = [
+    ("Sarah Good",       "Beggar",      39),
+    ("John Proctor",     "Farmer",      60),
+    ("Samuel Parris",    "Pastor",      39),
+    ("Rebecca Nurse",    "Housewife",   71),
+    ("Martha Corey",     "Seamstress",  72),
+    ("Thomas Danforth",  "Judge",       69),
+    ("Cotton Mather",    "Minister",    29),
+    ("Tituba",           "Storyteller", 35),
+    ("Abigail Williams", "Child",       11),
+    ("Ann Putnam",       "School Girl", 13),
+    ("Giles Corey",      "Landowner",   71),
+    ("Mary Warren",      "Servant",     17),
+    ("William Phips",    "Governor",    42),
+    ("George Burroughs", "Strongman",   40),
+    ("Will Griggs",      "Physician",   47)
+]
