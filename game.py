e3d8c8798fe7c6899f03ef322700600bc2054b10
Dan D <dandrust@gmail.com>
2024-02-13 21:46:21 -0500

    fix indentation

--- game.py
+++ game.py
@@ -13,41 +13,41 @@
 """
 
 class Game:
-  def __init__(self, player_count):
-    self.player_count = player_count
+    def __init__(self, player_count):
+      self.player_count = player_count
 
-  def not_a_witch_cards_required(self):
-    if self.player_count < 2 or self.player_count > 12:
-      return None
+    def not_a_witch_cards_required(self):
+        if self.player_count < 2 or self.player_count > 12:
+            return None
 
-    match self.player_count:
-      case 2 | 3 | 4:
-        return 18
-      case 5:
-        return 23
-      case 6 | 10:
-        return 27
-      case 7:
-        return 32
-      case 8:
-        return 29
-      case 9 | 12:
-        return 33
-      case 11:
-        return 30
+        match self.player_count:
+            case 2 | 3 | 4:
+                return 18
+            case 5:
+                return 23
+            case 6 | 10:
+                return 27
+            case 7:
+                return 32
+            case 8:
+                return 29
+            case 9 | 12:
+                return 33
+            case 11:
+                return 30
       
 
-  def witch_cards_required(self):
-    if self.player_count < 2 or self.player_count > 12:
-      return None
+    def witch_cards_required(self):
+        if self.player_count < 2 or self.player_count > 12:
+            return None
 
-    if self.player_count <= 5:
-      return 1
-    elif self.player_count > 5:
-      return 2
+        if self.player_count <= 5:
+            return 1
+        elif self.player_count > 5:
+            return 2
 
-  def constable_cards_required(self):
-    if self.player_count < 2 or self.player_count > 12:
-      return None
+    def constable_cards_required(self):
+        if self.player_count < 2 or self.player_count > 12:
+            return None
 
-    return 1
+        return 1
